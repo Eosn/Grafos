3261da409d23627b29646571990b955129f2592f
Éllen Neves <ellenosneves@gmail.com>
2023-03-14 14:05:48 -0300

    Update Vertice.java

--- Vertice.java
+++ Vertice.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 
 /*
- * @author Éllen Oliveira Silva Neves (20202BSI0071) e Carlos Breno Norato Rosa (20202BSI0233)
+ * @author Éllen Oliveira Silva Neves e Carlos Breno Norato Rosa
  */
 
 public class Vertice<T> {
